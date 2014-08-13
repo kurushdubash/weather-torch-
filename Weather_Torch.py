b51581b4758b2efd42dc4293260ab162dab09046
Kurush Dubash <dubash.kurush@gmail.com>
2014-08-13 16:54:10 -0700

    Added refresh rate, made infinite loop, fade of colors, added we

--- Weather_Torch.py
+++ Weather_Torch.py
@@ -1,15 +1,8 @@
 import requests
-
-# function = 'params'
-# access_id = '53ff6f065075535140441187'
-# access_token = 'e0dd630ae5f950ff67dad0c1cb20ae56e89dcd0b'
-
-# url = 'https://api.spark.io/v1/devices/' + access_id + '/' + function
-# data = {'access_token': access_token, 'args': 'red_energy=0'}
-
-# r = requests.post(url, data=data)
+import time
 
 api = '2ffd40362f6c4bdd050c1ad48eaa7891cb1e4890'
+ferinheight = True
 
 def get_zip_code():
 	print('Please enter your zip code:')
@@ -31,18 +24,65 @@ def get_temperature(json_data):
 	return temp
 
 def get_forcast(json_data):
-	forcast = json.data['data']['current_condition'][0]['weatherCode']
+	forcast = json_data['data']['current_condition'][0]['weatherCode']
 	return forcast
 
 def get_windspeed(json_data):
 	wind - json_data['data']['current_condition'][0]['windspeedMiles']
 	return str(wind)
 
+function = 'params'
+access_id = '53ff6f065075535140441187'
+access_token = 'e0dd630ae5f950ff67dad0c1cb20ae56e89dcd0b'
+
+url = 'https://api.spark.io/v1/devices/' + access_id + '/' + function
+
+def check_weather(value):
+	#API Pulls, Weather Data, and Weather codes
+	weather_json = get_weather_json(zip_code_input)
+	temp = get_temperature(weather_json)
+	weather_code = get_forcast(weather_json)
+
+	#Setting arguments for weather torch
+	value = value
+	args = 'upside_down=1,red_energy=' + str(value)
+	data = {'access_token': access_token, 'args': args}
+	r = requests.post(url, data=data)
+
+	sleep(15)
+
+
+def sleep(refresh_rate):
+	sleep_time = refresh_rate
+	while sleep_time > 0:
+		print(str(sleep_time), 'minutes remaining')
+		time.sleep(60)
+		sleep_time-=1
+
+
+# def change_colors():
+# 	while value > 0: 
+# 	if value - 20 < 0:
+# 		value = 0
+# 	else:
+# 		value-=20
+# 	args ='upside_down=1,red_energy=' + str(value)
+# 	data['args'] = args
+# 	print (args)
+# 	requests.post(url, data=data)
 
 zip_code_input = get_zip_code()
-weather_json = get_weather_json(zip_code_input)
+check_weather(255)
+while True:
+	check_weather(0)
 
-temp = get_temperature(weather_json)
 
-print(temp)
+
+
+
+
+
+
+
+
 
