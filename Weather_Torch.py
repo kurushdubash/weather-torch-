918786ecbe3aaa602606fb89932dc91605ac747e
Kurush Dubash <dubash.kurush@gmail.com>
2014-08-13 17:21:53 -0700

    Added Upside Down check and outline for RGB

--- Weather_Torch.py
+++ Weather_Torch.py
@@ -27,9 +27,9 @@ def get_forcast(json_data):
 	forcast = json_data['data']['current_condition'][0]['weatherCode']
 	return forcast
 
-def get_windspeed(json_data):
-	wind - json_data['data']['current_condition'][0]['windspeedMiles']
-	return str(wind)
+# def get_windspeed(json_data):
+# 	wind - json_data['data']['current_condition'][0]['windspeedMiles']
+# 	return str(wind)
 
 function = 'params'
 access_id = '53ff6f065075535140441187'
@@ -37,20 +37,37 @@ access_token = 'e0dd630ae5f950ff67dad0c1cb20ae56e89dcd0b'
 
 url = 'https://api.spark.io/v1/devices/' + access_id + '/' + function
 
-def check_weather(value):
+def check_weather():
 	#API Pulls, Weather Data, and Weather codes
 	weather_json = get_weather_json(zip_code_input)
 	temp = get_temperature(weather_json)
 	weather_code = get_forcast(weather_json)
 
+	print(weather_code)
 	#Setting arguments for weather torch
-	value = value
-	args = 'upside_down=1,red_energy=' + str(value)
+
+	red = 0
+	green = 0
+	blue = 0
+	is_upside_down = upside_down(int(weather_code))
+
+	args = ''
+	if is_upside_down:
+		args+= 'upside_down=1'
+	else: 
+		args+= 'upside_down=0'
+		
+	args = args + get_RGB()
+	print(args)
 	data = {'access_token': access_token, 'args': args}
 	r = requests.post(url, data=data)
 
 	sleep(15)
 
+def upside_down(weather_code):
+	if weather_code > 142:
+		return True
+	return False
 
 def sleep(refresh_rate):
 	sleep_time = refresh_rate
@@ -59,6 +76,8 @@ def sleep(refresh_rate):
 		time.sleep(60)
 		sleep_time-=1
 
+def get_RGB():
+	return 'red_energy=255'
 
 # def change_colors():
 # 	while value > 0: 
@@ -72,9 +91,9 @@ def sleep(refresh_rate):
 # 	requests.post(url, data=data)
 
 zip_code_input = get_zip_code()
-check_weather(255)
+check_weather()
 while True:
-	check_weather(0)
+	check_weather()
 
 
 
